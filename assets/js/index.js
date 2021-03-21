3fe6762b100f8e456496f208b84bc84dc38b3802
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-21 21:59:02 +0200

    Add typeof example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,24 @@
-alert("Hello World");
+//alert("Hello World");
+//alert("Bye, World");
 
-alert("Bye, World");
+const helloStr ='Hello World';
+let test = 'hello';
+
+let boolVariable = true;
+
+console.log(helloStr);
+
+console.log(test, typeof test);
+test = 5;
+console.log(test, typeof test);
+
+console.log(boolVariable, typeof boolVariable)
+
+//console.log('Hello World');
+//console.log(10);
+
+//console.log(true);
+//console.log(false);
+
+//console.log(null);
+//console.log(undefined);
